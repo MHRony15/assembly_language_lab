87b1c0542d0d08e2849bac4e22f3bbbe94dffdec
Rony <mahadyhasankbc710@gmail.com>
2019-12-31 01:40:50 +0600

    ok

---
+++ LabManual/Number_Processing/2A_Even_Odd_Number.asm
@@ -0,0 +1,42 @@
+.MODEL SMALL
+.STACK 100H
+.DATA 
+   MSG1 DB 'ENTER THE DECIMAL DIGIT: ','$'
+   MSG2 DB 0AH,0DH,'EVEN NUMBER ','$'
+   MSG3 DB 0AH,0DH,'ODD NUMBER ','$'  
+   R DB ?
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    LEA DX,MSG1
+    MOV AH,9
+    INT 21H 
+    
+    MOV AH,1
+    INT 21H 
+    MOV CL,AL
+    SUB CL,30H
+    MOV AX,CX 
+    MOV BL,2
+    DIV BL
+    MOV R,AH
+    CMP R,0
+    JE EVEN 
+    JMP ODD
+    
+    EVEN:
+    LEA DX,MSG2
+    MOV AH,9
+    INT 21H 
+    JMP EXIT
+    ODD:
+    LEA DX,MSG3
+    MOV AH,9
+    INT 21H
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP
+END
